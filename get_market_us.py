00c1df6e4b47edf66963c6a0ebaa570ead78747f
PongKool <pong118@gmail.com>
2026-09-07 20:04:13 +0700

    Update get_market_us.py

--- get_market_us.py
+++ get_market_us.py
@@ -16,21 +16,21 @@ import time
 # 1. INITIALIZE GLOBAL VARIABLES & CONFIGURATION
 my_costs = {
     "SNDK": 1561.62, 
-    "ORCL": 155.83, 
+    "ORCL": 156.08, 
     "AMZN": 0, 
-    "NVDA": 228.62, 
+    "NVDA": 228.84, 
     "AVGO": 0, 
     "SHOP": 152.14, 
-    "DIS": 107.85, 
+    "CCJ": 100.93, 
     "GOOG": 0, 
     "LRCX": 0,
-    "VRT": 0,
+    "VRT": 279.44,
     "MSFT": 509.02, 
-    "CEG": 284.89,
+    "CEG": 287.02,
     "NFLX": 0,
-    "TSM": 416.99,
+    "TSM": 419.35,
     "UBER": 0, 
-    "VST": 143.56    
+    "VST": 144.64    
 }
 
 tickers = list(my_costs.keys())
