e0e8fe795ab96bbe12661b508647e5a280447be8
PongKool <pong118@gmail.com>
2026-09-15 16:18:48 +0700

    Update get_market_th.py

--- get_market_th.py
+++ get_market_th.py
@@ -12,19 +12,19 @@ DRY_RUN = False  # Set to True when testing layout/code for $0.00; False for liv
 # 1. INITIALIZE GLOBAL VARIABLES & CONFIGURATION FIRST (THAI SET WATCHLIST)
 my_costs = {
     "BBL.BK": 193.32,       # Custom entries configured in THB
-    "GULF.BK": 67.25,
+    "GULF.BK": 67.41,
     "SCC.BK": 265.11,
-    "TU.BK": 13.28,
+    "TU.BK": 13.31,
     "AOT.BK": 0,
     "BCP.BK": 0,
     "BDMS.BK": 20.01,
     "KTB.BK": 44.54,
     "PTT.BK": 41.62,
     "TRUE.BK": 14.09,
-    "WHA.BK": 4.88,
+    "WHA.BK": 4.87,
     
-    "BH.BK": 197.67,
-    "ADVANC.BK": 0,
+    "BH.BK": 0,
+    "ADVANC.BK": 355.31,
     "GPSC.BK": 51.59,
     "OR.BK": 12.65,
     "IVL.BK": 25.32,
